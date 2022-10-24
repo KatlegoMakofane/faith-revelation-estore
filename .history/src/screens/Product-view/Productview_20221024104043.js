b36124db74c6466fc7aff681ccf82e555ff5ade0
Bongani <bonganicele806@gmail.com>
2022-10-24 13:52:51 +0200

    productview

---
+++ .history/src/screens/Product-view/Productview_20221024104043.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import Sdata from '../../screens/Sales/Sdata';
+import {TbBackspace} from 'react-icons/tb'
+import './Product.css'
+import { BsFillCartFill, BsFillHeartFill } from "react-icons/bs";
+
+const productview = () => {
+
+  // const {id} = useParams()
+  //   const [item, setItem] = useState(null)
+
+  //   useEffect(() => {
+  //       let item = Sdata.find((item) => item.id === parseInt(id))
+
+  //       if(item){
+  //           setItem(item)
+  //       }
+
+  //   }, [id] )
+
+
+  return (
+    <>
+      
+        <div className='Container'>
+        <Link to='/sales' className=''>
+                <TbBackspace size={50}/>
+            </Link>
+            <h1 className='heading'>Product View</h1>
+            <br></br>
+            <h2 className='heading2'>About Product</h2>
+           
+            <p className='name'>MASSDROP X SENNHEISER HD 6XX HEADPHONES</p>
+            <h4 className='heading4'>Description</h4>
+            <p className='desc'>ihuhljhijkjonjojnojonjijnmnuoujpiunoujoinnjhiuobinouobno</p>
+            <h4 className='heading4'>Available Sizes</h4>
+            <h4 className='heading4'>Available Colours</h4>
+            <button className='btnWish'>Add to Wishlist<BsFillCartFill size={45} className='icon' /></button>
+            <button className='btnAdd'> Add to Cart <BsFillCartFill size={45} className='icon' /></button>
+            
+               
+        </div>
+      
+    </>
+  )
+}
+
+export default productview
